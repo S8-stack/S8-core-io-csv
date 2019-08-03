43b6bea8e4e90ed2e029e314156e326d870a1eb5
pc <pc@pierres-macbook-pro.home>
2019-08-03 15:50:22 +0200

    module udpated

--- sources/module-info.java
+++ sources/module-info.java
@@ -7,9 +7,11 @@
  */
 module com.qx.io.csv {
 	
-	exports com.qx.base;
+	exports com.qx.io.csv;
+	exports com.qx.io.csv.basic;
 	exports com.qx.io.csv.mapped;
 	exports com.qx.io.csv.mapped.type;
 	
+	requires transitive com.qx.base;
 
 }
