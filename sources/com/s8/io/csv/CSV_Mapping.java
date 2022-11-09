78dc3ea8cfee8e4e5c0b51237618e420d8f82472
Pierre Convert <pierreconvert@macbook-pro-2.home>
2022-11-09 16:55:41 +0100

    updates

--- sources/com/s8/io/csv/CSV_Mapping.java
+++ sources/com/s8/io/csv/CSV_Mapping.java
@@ -8,6 +8,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * Use this method to declare that a method or a field msut be mapped to a .csv file column
+ * @author pierreconvert
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value={METHOD, FIELD, PARAMETER})
 public @interface CSV_Mapping {
