65e33097994f0ec1912e1820240faae862f8974b
Pierre Convert <pierreconvert@macbook-pro-2.home>
2022-11-12 10:37:38 +0100

    update

--- tests/com/s8/io/csv/tests/TestUnit02.java
+++ tests/com/s8/io/csv/tests/TestUnit02.java
@@ -10,7 +10,7 @@ import com.s8.io.csv.CSV_Engine;
 public class TestUnit02 {
 
 	public static void main(String[] args) throws Exception {
-		CSV_Engine<NominalPipeSize> engine = new CSV_Engine<>(NominalPipeSize.class, QxUnit.FACTORY);
+		CSV_Engine<NominalPipeSize> engine = new CSV_Engine<>(NominalPipeSize.class, QxUnit2.FACTORY);
 		InputStream inputStream = new FileInputStream(new File("data/nominal_pipe_sizes.csv"));
 		engine.forEachRow(inputStream, item -> {
 			System.out.println(item);
--- tests/com/s8/io/csv/tests/TestUnit03.java
+++ tests/com/s8/io/csv/tests/TestUnit03.java
@@ -11,7 +11,7 @@ public class TestUnit03 {
 
 	public static void main(String[] args) throws Exception {
 		CSV_Engine<ASME_B16_5_WeldingNeckFlangeModel> engine 
-			= new CSV_Engine<>(ASME_B16_5_WeldingNeckFlangeModel.class, QxUnit.FACTORY);
+			= new CSV_Engine<>(ASME_B16_5_WeldingNeckFlangeModel.class, QxUnit2.FACTORY);
 		InputStream inputStream = new FileInputStream(new File("data/ASME_B16.5_welded_neck_flanges.csv"));
 		engine.forEachRow(inputStream, item -> {
 			System.out.println(item);
